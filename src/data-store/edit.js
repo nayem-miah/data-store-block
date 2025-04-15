fb8fc13d1e2a3efa2ab5bfb10c99163eec8066cf
Nayem Miah <naeemislam13790@gmail.com>
2025-04-15 09:57:56 +0600

    'commited'

--- src/data-store/edit.js
+++ src/data-store/edit.js
@@ -7,7 +7,6 @@ import { useSelect, useDispatch } from '@wordpress/data';
 export default function Edit() {
 	const title = useSelect((select) => {
 		return select('core/editor')
-
 			.getEditedPostAttribute('title');
 	});
 	const { editPost } = useDispatch('core/editor');
